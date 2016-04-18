4418574fc371f5d637e73a02df04cd3d325202eb
patryk1303 <patryk1303@gmail.com>
2016-04-18 09:31:14 +0200

    Intro and error texts

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,6 +1,9 @@
 app.controller('MainController', ($scope, Cnx, Lines) => {
   let cnx
   $scope.checking = false
+  $scope.success = false
+  $scope.error = false
+  $scope.errorMessage = null
 
   // debug info
   $scope.host = 'localhost'
@@ -15,10 +18,16 @@ app.controller('MainController', ($scope, Cnx, Lines) => {
         console.log(_cnx)
         cnx = _cnx
         $scope.checking = false
+        $scope.success = true
+        $scope.error = false
+        $scope.errorMessage = null
       })
       .catch((err) => {
         console.error(err)
         $scope.checking = false
+        $scope.success = false
+        $scope.error = true
+        $scope.errorMessage = err.code
       })
   }
 })
